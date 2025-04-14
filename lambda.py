50405f0b8bb46f53c73ea4744252cc750903156c
Sai Madan Mohan <chalamalasetti27@gmail.com>
2025-04-14 10:36:03 +0530

    lambda

---
+++ lambda.py
@@ -0,0 +1,19 @@
+
+add = lambda a, b: a + b
+print(add(5, 3))  # Output: 8
+
+square = lambda x: x * x
+print(square(4))  # Output: 16
+
+pairs = [(1, 5), (3, 1), (10, 2)]
+pairs.sort(key=lambda x: x[1])
+print(pairs)  # Output: [(3, 1), (10, 2), (1, 5)]
+
+nums = [1, 2, 3, 4, 5, 6]
+evens = list(filter(lambda x: x % 2 == 0, nums))
+print(evens)  # Output: [2, 4, 6]
+
+print(add(9,10))
+
+print(square(6))
+
