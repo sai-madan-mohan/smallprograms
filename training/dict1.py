eb44b424a375b8da5100299a855cb827eda01c14
Sai Madan Mohan <chalamalasetti27@gmail.com>
2025-05-02 12:50:36 +0530

    dict problems

---
+++ training/dict1.py
@@ -0,0 +1,24 @@
+
+suits = {
+    "name":"mark45",
+    "year":2024,
+    "specification":"Arc"
+}
+
+# accessing the elements
+print(suits["year"])
+
+# replacing
+suits["year"]=2025
+print(suits)
+
+
+suits = {"mark45":{"year":2024,"tech":"Arc"},
+            "mark90":{"year":2025,"tech":"Nano"}}
+print(suits["mark90"]["tech"])
+
+# mergeing two dictionaries
+suit_a = {"mark45":2018,"mark90":2022}
+suit_b = {"mark100":2023,"hulkbuster":2024}
+suit_a.update(suit_b)
+print(suit_a)
