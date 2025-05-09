23c685fcc9788b83c29b17bde64ff1f4c40b64bb
Sai Madan Mohan <chalamalasetti27@gmail.com>
2025-05-09 22:22:15 +0530

    if else

--- training/dict1.py
+++ training/dict1.py
@@ -1,25 +1,31 @@
 
-suits = {
-    "name":"mark45",
-    "year":2024,
-    "specification":"Arc"
-}
+# suits = {
+#     "name":"mark45",
+#     "year":2024,
+#     "specification":"Arc"
+# }
 
-# accessing the elements
-print(suits["year"])
+# # accessing the elements
+# print(suits["year"])
 
-# replacing
-suits["year"]=2025
-print(suits)
+# # replacing
+# suits["year"]=2025
+# print(suits)
 
 
-suits = {"mark45":{"year":2024,"tech":"Arc"},
-            "mark90":{"year":2025,"tech":"Nano"}}
-print(suits["mark90"]["tech"])
+# suits = {"mark45":{"year":2024,"tech":"Arc"},
+#             "mark90":{"year":2025,"tech":"Nano"}}
+# print(suits["mark90"]["tech"])
 
-# mergeing two dictionaries
-suit_a = {"mark45":2018,"mark90":2022}
-suit_b = {"mark100":2023,"hulkbuster":2024}
-suit_a.update(suit_b)
-print(suit_a)
+# # mergeing two dictionaries
+# suit_a = {"mark45":2018,"mark90":2022}
+# suit_b = {"mark100":2023,"hulkbuster":2024}
+# suit_a.update(suit_b)
+# print(suit_a)
 
+
+age= 20
+if age<18:
+    print("you are not eligible to vote")
+else:
+    print("you are eligible to vote")
